fc50073df2d7fe75aff155a924a06571a0248310
the-shivam-gupta <shivamgupta02022002@gmail.com>
2026-08-11 11:35:12 +0530

    Added GTM code within layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -42,8 +42,27 @@ export default async function RootLayout({ children }) {
           httpEquiv="Content-Security-Policy"
           content="upgrade-insecure-requests"
         />
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TC56DRFT');`}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TC56DRFT"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ThemeInit />
         <Header />
         {children}
